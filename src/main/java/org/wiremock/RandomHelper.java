4f3b6ad77850608cba4b08c9d36cc683866823d9
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2024-05-02 11:57:50 -0400

    Adds support for seed hash.

--- src/main/java/org/wiremock/RandomHelper.java
+++ src/main/java/org/wiremock/RandomHelper.java
@@ -2,12 +2,15 @@ package org.wiremock;
 
 import com.github.jknack.handlebars.Options;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HandlebarsHelper;
+import java.util.Random;
 import net.datafaker.Faker;
 
 /*
  * Author: Shreya Agarwal
  */
 public class RandomHelper extends HandlebarsHelper<Object> {
+
+  private static final String SEED_HASH = "seed";
 
   private final Faker faker;
 
@@ -17,13 +20,18 @@ public class RandomHelper extends HandlebarsHelper<Object> {
 
   @Override
   public Object apply(Object context, Options options) {
+
+    // Use a seeded Faker if seed hash exists
+    final var seedOption = options.hash(SEED_HASH);
+    var myFaker = seedOption == null ? faker : new Faker(new Random(seedOption.hashCode()));
+
     try {
       // Used the `expression` method instead of `resolve` as the
       // resolve method is not able to resolve nested references in the yml files. For example, in
       // the resource file for en-US, `address.full_address` wasn't resolving because of the
       // `#{street_address}` nested reference, but `address.postcode_by_state.AL` would work fine.
       // `expression` is able to handle all cases.
-      return faker.expression("#{" + context + "}");
+      return myFaker.expression("#{" + context + "}");
     } catch (RuntimeException e) {
       return handleError("Unable to evaluate the expression " + context, e);
     }
