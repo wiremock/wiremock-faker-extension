9d3607926abe189e6fce0b42137d7274033311d3
Jérôme Ruillier <jerome.ruillier@aronnax.eu>
2024-05-02 15:38:23 -0400

    Adds support for locale hash.

--- src/main/java/org/wiremock/RandomHelper.java
+++ src/main/java/org/wiremock/RandomHelper.java
@@ -4,6 +4,7 @@ import com.github.jknack.handlebars.Options;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HandlebarsHelper;
 import java.util.Random;
 import net.datafaker.Faker;
+import org.apache.commons.lang3.LocaleUtils;
 
 /*
  * Author: Shreya Agarwal
@@ -11,19 +12,25 @@ import net.datafaker.Faker;
 public class RandomHelper extends HandlebarsHelper<Object> {
 
   private static final String SEED_HASH = "seed";
+  private static final String LOCALE_HASH = "locale";
 
+  private final Random random;
   private final Faker faker;
 
   public RandomHelper() {
-    faker = new Faker();
+    this(new Random());
+  }
+
+  RandomHelper(Random random) {
+    this.random = random;
+    this.faker = new Faker(random);
   }
 
   @Override
   public Object apply(Object context, Options options) {
 
-    // Use a seeded Faker if seed hash exists
-    final var seedOption = options.hash(SEED_HASH);
-    var myFaker = seedOption == null ? faker : new Faker(new Random(seedOption.hashCode()));
+    // Use a seeded/localized/default Faker if `locale` or `seed` hash exist
+    final Faker myFaker = this.getFaker(options.hash(SEED_HASH), options.hash(LOCALE_HASH));
 
     try {
       // Used the `expression` method instead of `resolve` as the
@@ -35,5 +42,19 @@ public class RandomHelper extends HandlebarsHelper<Object> {
     } catch (RuntimeException e) {
       return handleError("Unable to evaluate the expression " + context, e);
     }
+  }
+
+  private Faker getFaker(Object seed, String locale) {
+    final Faker myFaker;
+    if (seed != null && locale != null) {
+      myFaker = new Faker(LocaleUtils.toLocale(locale), new Random(seed.hashCode()));
+    } else if (seed != null) {
+      myFaker = new Faker(new Random(seed.hashCode()));
+    } else if (locale != null) {
+      myFaker = new Faker(LocaleUtils.toLocale(locale), this.random);
+    } else {
+      myFaker = this.faker;
+    }
+    return myFaker;
   }
 }
